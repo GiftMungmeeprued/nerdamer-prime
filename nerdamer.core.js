7f2594249525effe62006ab8eafbc4156c814461
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-09-25 14:27:43 +0100

    fix divide

--- nerdamer.core.js
+++ nerdamer.core.js
@@ -7066,7 +7066,7 @@ var nerdamer = (function (imports) {
                                 this.callPeekers('pre_operator', a, b, e);
 
                                 var ans;
-                                if(e.action==='divide' && a.isPoly() && b.isPoly())
+                                if(e.action==='divide' && a.isPoly() && b.isPoly() && variables(a).length>0 && variables(b).length>0)
                                     ans = _.callfunction(e.action, [a,b]);
                                 else
                                     ans = _[e.action](a, b);
